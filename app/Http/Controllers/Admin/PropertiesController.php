46fb90755babea30a7805ce3a080ceb599f2dae9
Hok Vichaka <hvicheka.it@gmail.com>
2022-07-16 08:30:25 +0700

    Update Home Page

--- app/Http/Controllers/Admin/PropertiesController.php
+++ app/Http/Controllers/Admin/PropertiesController.php
@@ -3,16 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Inventery;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Image;
-<<<<<<< HEAD
-use App\Models\User;
-use App\Models\Properties;
-=======
 use App\Models\Property;
->>>>>>> f7be6bcd2334dd028c888476cfff9d9262bbd201
 use App\Models\location\CityProvince;
 use App\Models\location\District;
 use App\Models\location\Commune;
@@ -27,14 +21,9 @@ class PropertiesController extends Controller
      */
     public function index()
     {
-// <<<<<<< HEAD
-//         $properties=Properties::all();
-//         return redirect()->route('admin.dashboard')->with('properties','property Create succassfully');
-// =======
-//         $properties = Property::all();
+        $properties = Property::all();
+        return redirect()->route('admin.dashboard')->with('properties', 'property Create succassfully');
 
-//         return redirect()->route('admin.dashboard')->with('properties', 'property Create succassfully');
-// >>>>>>> d54922a55117e9c725aa13321098a58991b429c3
     }
 
     /**
